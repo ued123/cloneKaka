a7b0e3b0a0f06acec83094bebb72b4e50d86bfd9
seongheeon <ued_123@naver.com>
2021-03-01 14:58:56 +0900

    카카오 클론 코딩 프로젝트 sinse - 2021.03.01

---
+++ src/main/java/com/example/clone/CloneKakaApplication.java
@@ -0,0 +1,13 @@
+package com.example.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneKakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneKakaApplication.class, args);
+    }
+
+}
